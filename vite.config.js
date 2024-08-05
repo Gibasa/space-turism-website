838687619f0623e8e07cc924737ebc7c90ef91ee
Gilberto Sá <gibasa@gmail.com>
2024-08-05 10:10:30 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Gibasa/space-turism-website",
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: "/space-turism-website",
 })
